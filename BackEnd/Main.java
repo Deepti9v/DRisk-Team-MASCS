8861871ba23b59d0f43300bd1038b88628a41310
mengjunglin <candy800419@yahoo.com.tw>
2015-09-29 04:43:56 +0800

    Setup
    Renaming filename.
    Adding file containing common methods.

---
+++ BackEnd/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) 
+    {
+        //This block will be used to test the functions works properly
+    }
+}
---
+++ BackEnd/attack.java
@@ -0,0 +1,8 @@
+public class Attack {
+	//testing
+	public String a = "Attack";
+	public String getA()
+	{
+		return a;
+	}
+}
